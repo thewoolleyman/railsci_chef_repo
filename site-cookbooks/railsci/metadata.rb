4642363f26944070be8ed33be19ae3e4e3496ad7
Chad Woolley <thewoolleyman@gmail.com>
2010-05-22 21:51:27 -0700

    create railsci cookbook

---
+++ site-cookbooks/railsci/metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "Chad Woolley"
+maintainer_email "thewoolleyman@opscode.com"
+license          "Apache 2.0"
+description      "Installs a CI server for the Ruby on Rails framework's source code"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1.0"
+
+%w{ubuntu debian}.each do |os|
+  supports os
+end
+
+%w{runit}.each do |cb|
+  depends cb
+end
