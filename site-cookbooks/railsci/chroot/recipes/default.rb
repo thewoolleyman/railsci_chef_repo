4640703bc7cf317a4722e39b33ff6c8a688e4fc5
Chad Woolley <thewoolleyman@gmail.com>
2010-05-24 21:10:45 -0700

    skip mysql grants in chroot

--- site-cookbooks/railsci/chroot/recipes/default.rb
+++ site-cookbooks/railsci/chroot/recipes/default.rb
@@ -17,29 +17,20 @@
 # limitations under the License.
 #
 
+collection.all_resources.each do |resource|
+  if resource.to_s == 'template[/etc/mysql/grants.sql]'
+    resource.actions[:run][:delayed].clear
+  end
+end
+
 collection.each do |r|
  # Switch every service resource off
  r.action :nothing if r.resource_name == :service
  # Find all notified/subscribed actions on services and switch them off too
  r.actions.each do |action, timings|
-   p 11111111111111111
-   p action
    timings.each do |timing, resources|
-     p 2222222222
-     p timing
      resources.each do |notified_resource|
-       p 33333333333333
-
-       p notified_resource.cookbook_name
-       p notified_resource.recipe_name
-       p notified_resource.resource_name
-       p notified_resource.action
-       p notified_resource.actions
-       p notified_resource.allowed_actions
-       p notified_resource.node
-
-       notified_resource.action :nothing if
-notified_resource.resource_name == :service
+       notified_resource.action :nothing if notified_resource.resource_name == :service
      end
    end
  end
